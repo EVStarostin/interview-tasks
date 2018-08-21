b9151c2671c7d136123a59c7c5c512845bc911ec
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-21 18:10:24 +0300

    -

--- YA/task4.js
+++ YA/task4.js
@@ -1,6 +1,9 @@
 // 4. Написать функцию getAnagrams
 function getAnagrams() {
-    
+  for (let i = 0; i < arguments.length; i++) {
+    const argument = arguments[i];
+    console.log(argument);
+  }
 }
 
-getAnagrams(“нос”, “сон”, “снедь”, “днесь”)
+getAnagrams("нос", "сон", "снедь", "днесь");
