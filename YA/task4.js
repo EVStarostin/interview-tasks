71ee08edf7f04c70254dc4472d36a00b7e3dfaff
Evgenii Starostin <e.starostin89@gmail.com>
2018-08-21 21:00:58 +0300

    4 задание

--- YA/task4.js
+++ YA/task4.js
@@ -1,9 +1,35 @@
 // 4. Написать функцию getAnagrams
 function getAnagrams() {
-  for (let i = 0; i < arguments.length; i++) {
-    const argument = arguments[i];
-    console.log(argument);
+  const sortAlphabetically = (str) => {
+    return str.split('').sort().join('');
   }
+
+  const args = Array.prototype.slice.call(arguments);
+  const sortedArguments = args.map( el => sortAlphabetically(el) );
+
+  const anagrams = [];
+  for (let i = 0; i < args.length; i++) {
+    const sortedArgument = sortAlphabetically(args[i]);
+    
+    const curAnagrams = [];
+    let idx = sortedArguments.indexOf(sortedArgument, i);
+    while (idx != -1) {
+      curAnagrams.push(args[idx]);
+      delete sortedArguments[idx];
+      idx = sortedArguments.indexOf(sortedArgument, idx + 1);
+    }
+
+    if (curAnagrams.length > 1) anagrams.push(curAnagrams);
+  }
+
+  return anagrams;
 }
 
-getAnagrams("нос", "сон", "снедь", "днесь");
+const x = getAnagrams("нос", "сон", "снедь", "днесь");
+console.log(JSON.stringify(x));
+/* 
+[
+  ["нос", "сон"],
+  ["днесь", "снедь"]
+] 
+*/
