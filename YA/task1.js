c63bf378f788d72bcb4cfd5f147872af7b0129a8
Starostin Evgenii <estarostin@bellintegrator.com>
2018-08-21 17:01:10 +0300

    1 задача

---
+++ YA/task1.js
@@ -0,0 +1,11 @@
+// 1. Написать функцию getPrimes(n) // Должна вернуть простые числа от 2 до n;
+function getPrimes(n) {
+    const primesArray = [];
+    for (let i = 2; i <= n; i += 2) {
+      primesArray.push(i);
+    }
+    return primesArray;
+  }
+  
+  const x = getPrimes(10);
+  console.log(x.join('; '));
